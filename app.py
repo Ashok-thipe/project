8fb00065d7a7e94179812663206d8e177e8d6720
Ashok-thipe <ashokthipe912@gmail.com>
2026-03-03 15:39:54 +0530

    Added TO_DO frontend page to master1

--- app.py
+++ app.py
@@ -1,15 +1,5 @@
-from flask import Flask, jsonify
-import json
+from flask import Flask, render_template
 
-app = Flask(__name__)
-
-DATA_FILE = "data.json"
-
-@app.route("/api", methods=["GET"])
-def get_data():
-    with open(DATA_FILE, "r") as file:
-        data = json.load(file)
-    return jsonify(data)
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/todo")
+def todo():
+    return render_template("todo.html")
